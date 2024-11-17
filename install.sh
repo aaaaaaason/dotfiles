652948e299650e1a0223e96c4882faa136ca88d4
aaaaaaason <aaaaaaason@gmail.com>
2024-11-17 15:42:20 +0800

    support osc52 in tmux

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 WORKDIR=$HOME/Projects
 
-# nerd font
-NERDDIR=$WORKDIR/nerd-fonts
-if [[ ! -d "${WORKDIR}/nerd-fonts" ]]; then
-	echo "download nerd fonts..."
-	git clone --depth=1 https://github.com/ryanoasis/nerd-fonts.git $NERDDIR
-	$NERDDIR/install.sh JetBrainsMono
-fi
 
 # tmux
 TPMDIR=$HOME/.tmux/plugins/tpm
@@ -18,11 +11,3 @@ fi
 mkdir -p $TMUXDIR
 ln ./tmux/tmux.conf $TMUXDIR
 
-# lazyvim
-NVIMDIR=$HOME/.config/nvim
-if [[ ! -d "$NVIMDIR" ]]; then
-	echo "download lazyvim starter..."
-	git clone --depth=1 https://github.com/LazyVim/starter.git $NVIMDIR
-fi
-ln ./lazyvim/{lazy-lock.json,lazyvim.json} $NVIMDIR
-ln ./lazyvim/plugins.lua $NVIMDIR/lua/plugins
